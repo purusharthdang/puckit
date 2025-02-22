1406e4cc552458c3c01196b8985b0688dbe8b803
Purusharth Dang <purusharthdang@Purusharths-Mac-mini.local>
2025-02-22 10:23:58 +0530

    #1 - First commit with puck setup and tailwind

--- app/puck/[...puckPath]/client.tsx
+++ app/puck/[...puckPath]/client.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import type { Data } from "@measured/puck";
-import { Puck } from "@measured/puck";
+import { Button, Puck } from "@measured/puck";
 import config from "../../../puck.config";
+import Link from "next/link";
 
 export function Client({ path, data }: { path: string; data: Partial<Data> }) {
+
   return (
     <Puck
       config={config}
@@ -14,6 +16,21 @@ export function Client({ path, data }: { path: string; data: Partial<Data> }) {
           method: "post",
           body: JSON.stringify({ data, path }),
         });
+      }}
+      overrides={{
+        headerActions: ({ children }) => (
+          <>
+            <div>
+              <Link href={'/'}>
+                <Button variant="secondary">
+                  View page
+                </Button>
+              </Link>
+            </div>
+
+            {children}
+          </>
+        ),
       }}
     />
   );
